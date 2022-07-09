3f6a6d8fed41409b9e7f3ac126e5d146f3275b14
Shogo0607 <soffionam@yahoo.co.jp>
2022-07-09 15:10:12 +0900

    first

--- Search.py
+++ Search.py
@@ -2,6 +2,6 @@ from selenium import webdriver
 from webdriver_manager.firefox import GeckoDriverManager
 import streamlit as st
 
-with st.spinner("立ち上げ中...")
+with st.spinner("立ち上げ中..."):
     driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
     st.success("success")
