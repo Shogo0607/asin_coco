a5a901f70a7925f2fab74f8bb0256ce3293dfe03
Shogo0607 <soffionam@yahoo.co.jp>
2022-07-09 15:16:16 +0900

    first

--- Search.py
+++ Search.py
@@ -1,7 +1,13 @@
 from selenium import webdriver
-from selenium.webdriver.firefox.service import Service as FirefoxService
-from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.chrome import ChromeDriverManager
+
 import streamlit as st
 with st.spinner("立ち上げ中..."):
-    driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')  
+    chrome_options.add_argument('--disable-dev-shm-usage') 
+    # chrome_options.add_experimental_option('excludeSwitches', ['enable-automation', 'enable-logging'])
+    print('>処理開始')
+    driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
     st.success("success")
